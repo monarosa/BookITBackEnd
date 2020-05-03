f34633b7041cee5c4833062aacf55eebbf3d6155
Caglar, Ridvan <rukal.61@gmail.com>
2020-05-02 22:50:48 -0400

    DeleteStudent.feature and GetRooms.feature added

--- src/test/java/step_definitions/APIStepDefinitions.java
+++ src/test/java/step_definitions/APIStepDefinitions.java
@@ -6,12 +6,16 @@ import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Assert;
+import pojos.Room;
 import utilities.APIUtilities;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.contains;
 
 public class APIStepDefinitions {
     private String token;
@@ -58,7 +62,77 @@ public class APIStepDefinitions {
     @Then("user verifies that response status code is {int}")
     public void user_verifies_that_response_status_code_is(int expected) {
         Assert.assertEquals(expected, response.statusCode());
+
     }
 
 
+    @When("user sends GET request to {string}")
+    public void user_sends_GET_request_to(String string) {
+        response = given().
+                accept(contentType).
+                auth().oauth2(token).
+                when().
+                get(string).prettyPeek();
+    }
+
+    @Then("user should be able to see {int} rooms")
+    public void user_should_be_able_to_see_rooms(int expected) {
+        List<Object> rooms = response.jsonPath().get();
+
+        Assert.assertEquals( expected, rooms.size());
+    }
+
+
+    @Then("user should be able to see all room names")
+    public void user_should_be_able_to_see_all_room_names() {
+        List<Room> rooms = response.jsonPath().getList("", Room.class);
+//          will not work because list is Unmodifiable
+//        so we cannot change order
+      //  Collections.sort(rooms);
+
+        for (Room room : rooms) {
+            System.out.println(room.getName());
+        }
+
+        System.out.println("#########AFTER SORTING##########");
+        //create new list to overcome the issue with Unmodifiable list
+        List<Room> rooms2 = new ArrayList<>(rooms);
+
+        Collections.sort(rooms2);
+
+        for (Room room : rooms2) {
+            System.out.println(room.getName());
+        }
+    }
+    @Then("user payload contains following room names:")
+    public void user_payload_contains_following_room_names(List<String> dataTable) {
+
+        List<String> actualRoomNames = response.jsonPath().getList("name");
+
+        Assert.assertTrue(actualRoomNames.containsAll(dataTable));
+    }
+    @Then("user deletes previously added students")
+    public void user_deletes_previously_added_students(List<Map<String, String>> students) {
+        for (Map<String, String> student : students) {
+            response = APIUtilities.deleteMe(student.get("email"), student.get("password"));
+        }
+    }
+
+    @When("user sends DELETE request to {string} to exclude student")
+    public void user_sends_DELETE_request_to_to_exclude_student(String string) {
+        response = given().
+                accept(contentType).auth().oauth2(token).
+                when().
+                delete(string).prettyPeek();
+    }
+
+    @When("user verifies that status line contains {string}")
+    public void user_verifies_that_status_line_contains(String string) {
+        Assert.assertTrue(response.statusLine().contains(string));
+    }
+
+    @When("user verifies that payload contains {string} message")
+    public void user_verifies_that_payload_contains_message(String string) {
+        response.then().assertThat().body("", contains(string));
+    }
 }
--- src/test/resources/features/api/CreateStudent.feature
+++ src/test/resources/features/api/CreateStudent.feature
@@ -1,6 +1,6 @@
-
+@api @create_student
 Feature: Create student
-  @api
+
 Scenario: 1. Create student as a team member and verify status code 422
 Given authorization token is provided for "teacher"
 And user accepts content type as "application/json"
@@ -8,3 +8,15 @@ When user sends POST request to "/api/students/student" with following informati
 | first-name | last-name | email             | password | role                | campus-location | batch-number | team-name      |
 | Les      | McDonald  | lesslee@email.com | 1111     | student-team-member | VA              | 12           | Online_Hackers |
 And user verifies that response status code is 403
+
+  @berr
+  Scenario: 2. Create student as a teacher and verify status code 201
+    Given authorization token is provided for "teacher"
+    And user accepts content type as "application/json"
+    When user sends POST request to "/api/students/student" with following information:
+      | first-name | last-name | email               | password | role                | campus-location | batch-number | team-name      |
+      | Lesly      | SDET      | lesly2020@email.com | 1111     | student-team-member | VA              | 12           | Online_Hackers |
+    And user verifies that response status code is 201
+    Then user deletes previously added students
+      | first-name | last-name | email               | password | role                | campus-location | batch-number | team-name      |
+      | Lesly      | SDET      | lesly2020@email.com | 1111     | student-team-member | VA              | 12           | Online_Hackers |
